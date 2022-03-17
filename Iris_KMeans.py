a8c181e6c024e397a52d92f706650624d081ec7b
Nathan Smallcalder <74361286+NathanSmallcalder@users.noreply.github.com>
2022-03-17 22:20:07 +0000

    K-Means
    Added Titles to the dataset
    Imported New K means Algorithm - https://www.analyticsvidhya.com/blog/2021/04/k-means-clustering-simplified-in-python/

--- Iris_KMeans.py
+++ Iris_KMeans.py
@@ -3,46 +3,21 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.datasets._samples_generator import make_blobs
 from sklearn.cluster import KMeans
-import csv
 
-with open('iris.csv', newline='') as csvfile:
-    data = list(csv.reader(csvfile))
+data = pd.read_csv('Iris\iris.csv')
 
-SepalLength_Setosa = []
-SepalLength_Versicolor = []
-SepalLength_Virginica =[]
+plt.xlim(0,10)
+plt.ylim(0,10)
 
-PetalLength_Setosa = []
-PetalLength_Versicolor = []
-PetalLength_Virginica =[]
+x = data.iloc[:,1:3]
+kmeans = KMeans(3)
+kmeans.fit(x)
+identified_clusters = kmeans.fit_predict(x)
+print(identified_clusters)
 
-i = 0
-#Organising SepalLength Data
-while i < len(data) -1:
-    if(data[i][4] == 'Iris-setosa'):
-        SepalLength_Setosa.append(data[i][0])
-    if(data[i][4] == 'Iris-versicolor'):
-        SepalLength_Versicolor.append(data[i][0])
-    if(data[i][4] == 'Iris-virginica'):
-        SepalLength_Virginica.append(data[i][0])
-    i = i + 1
-
-i = 0
-#Organising PetalLength Data
-while i < len(data) -1:
-    if(data[i][4] == 'Iris-setosa'):
-        PetalLength_Setosa.append(data[i][2])
-    if(data[i][4] == 'Iris-versicolor'):
-        PetalLength_Versicolor.append(data[i][2])
-    if(data[i][4] == 'Iris-virginica'):
-        PetalLength_Virginica.append(data[i][2])
-    i = i + 1
-
-print(SepalLength_Setosa)
-X, y = make_blobs(n_samples=5, centers=4, cluster_std=0.60, random_state=0)
-kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
-pred_y = kmeans.fit_predict(X)
-plt.scatter(PetalLength_Setosa,SepalLength_Setosa)
-plt.scatter(PetalLength_Versicolor,PetalLength_Versicolor)
-plt.scatter(SepalLength_Virginica,PetalLength_Virginica)
+data_with_clusters = data.copy()
+data_with_clusters['Clusters'] = identified_clusters 
+plt.scatter(data_with_clusters['PetalLength'],data_with_clusters['SepalLength'],c=data_with_clusters['Clusters'],cmap='rainbow')
+plt.xlabel('Petal Length')
+plt.ylabel('Sepal Length')
 plt.show()
